ecac87a1a28011d40baea31531d0d1de2c539a3a
christine <christinewasabi@gmail.com>
2023-08-04 13:13:37 +0300

    sc-85912:android cordova support

--- src/android/com/movableink/app/MovablePlugin.kt
+++ src/android/com/movableink/app/MovablePlugin.kt
@@ -8,68 +8,69 @@ import com.movableink.app.ORDER_COMPLETED
 import com.movableink.app.PRODUCT_ADDED
 import com.movableink.app.PRODUCT_SEARCHED
 import com.movableink.app.PRODUCT_VIEWED
-import com.movableink.app.RESOLVE_URL
 import com.movableink.app.RETRIEVE_LAST_RESOLVED_URL
 import com.movableink.app.SET_MIU
-import com.movableink.app.START
 import com.movableink.inked.MIClient
-
 import org.apache.cordova.CallbackContext
+import org.apache.cordova.CordovaInterface
 import org.apache.cordova.CordovaPlugin
+import org.apache.cordova.CordovaWebView
+import org.apache.cordova.PluginResult
 import org.json.JSONArray
 import org.json.JSONException
 
-class MovablePlugin: CordovaPlugin {
-  var mDeepLinkListener: CallbackContext?
+const val TAG = "MovablePlugin"
 
-  override fun onNewIntent(intent: Intent?) {
-    super.onNewIntent(intent)
-    cordova.getActivity().setIntent(intent)
-    handleIntent(intent)
-  }
-  
-  fun initialize(cordova: CordovaInterface?, webView: CordovaWebView?) {
+class MovablePlugin : CordovaPlugin() {
+
+  override fun initialize(cordova: CordovaInterface?, webView: CordovaWebView?) {
+    super.initialize(cordova, webView)
+    handleIntent(cordova?.activity?.intent)
     MIClient.start()
-    
-    handleIntent(cordova.getActivity().getIntent());
   }
-  
+
   private fun handleIntent(intent: Intent?) {
-    if (intent == null) return
-
-    val action = intent.getAction()
-    val launchURI = intent.getData()
-
-    if (!Intent.ACTION_VIEW.equals(action) || launchURI == null) return
-
-    val result = PluginResult(PluginResult.Status.OK, resolvedURL)
-    
-    MIClient.resolveUrlAsync(launchURI) { resolvedURL ->
-      val result = PluginResult(PluginResult.Status.OK, resolvedURL)
-      result.setKeepCallacK(true)
-      
-      if (mDeepLinkListener == null) return
-
-      mDeepLinkListener.sendPluginResult(result)
+    intent?.let {
+      if (it.action == Intent.ACTION_VIEW) {
+        val url = it.data.toString()
+        resolveURL(url)
+      }
     }
   }
+  override fun onNewIntent(intent: Intent?) {
+    super.onNewIntent(intent)
+    handleIntent(intent)
+  }
 
-  override fun execute(action: String, args: JSONArray, callback: CallbackContext): Boolean {
+  private fun resolveURL(url: String): String? {
+    var resolvedLink: String? = null
+    MIClient.resolveUrlAsync(url) { urlString ->
+      // callBack
+      resolvedLink = urlString
+      Log.d(TAG, "Resolved URL:$resolvedLink ")
+//            val result = PluginResult(PluginResult.Status.OK, resolvedLink)
+//            callbackContext.sendPluginResult(result)
+    }
+    return resolvedLink
+  }
+
+  override fun execute(
+    action: String,
+    args: JSONArray,
+    callbackContext: CallbackContext,
+  ): Boolean {
     when (action) {
       SET_MIU -> {
         return setMiu(args)
       }
-      RESOLVE_URL -> {
-        return resolveUrl(args, callback)
-      }
       ORDER_COMPLETED -> {
-        return orderCompleted(args)
+        return orderCompleted(args, callbackContext)
       }
       PRODUCT_VIEWED -> {
-        return productViewed(args)
+        return productViewed(args, callbackContext)
       }
       CATEGORY_VIEWED -> {
-        return categoryViewed(args)
+        return categoryViewed(args, callbackContext)
       }
       PRODUCT_SEARCHED -> {
         return productSearched(args)
@@ -81,25 +82,15 @@ class MovablePlugin: CordovaPlugin {
         return identifyUser()
       }
       RETRIEVE_LAST_RESOLVED_URL -> {
-        return lastResolvedUrl(args, callback)
+        return lastResolvedUrl(callbackContext)
       }
       LOG_EVENT -> {
         return logEvent(args)
       }
-      START -> {
-        return start(callback)
-      }
-      else -> {
-        callback.error("Invalid action")
-      }
     }
+    return false
   }
-  
-  private fun start(callback: CallbackContext): Boolean {
-    mDeepLinkListener = callback
-    return true
-  }
-  
+
   private fun setMiu(parameters: JSONArray): Boolean {
     val miu: String? = try {
       parameters.getString(0)
@@ -107,20 +98,59 @@ class MovablePlugin: CordovaPlugin {
       e.printStackTrace()
       return true
     }
-    
     if (miu.isNullOrEmpty()) {
       return true
     }
-    
     MIClient.setMIU(miu)
     return true
   }
-  
+  private fun orderCompleted(parameters: JSONArray, callbackContext: CallbackContext): Boolean {
+    val properties = parameters.readProperties()
+    MIClient.orderCompleted(properties)
+    val pluginResult = PluginResult(PluginResult.Status.OK)
+    pluginResult.keepCallback = false
+    callbackContext.sendPluginResult(pluginResult)
+    return true
+  }
+  private fun productViewed(parameters: JSONArray, callbackContext: CallbackContext): Boolean {
+    val properties = parameters.readProperties()
+    MIClient.productViewed(properties)
+    val pluginResult = PluginResult(PluginResult.Status.OK)
+    pluginResult.keepCallback = false
+    callbackContext.sendPluginResult(pluginResult)
+    return true
+  }
+  private fun categoryViewed(parameters: JSONArray, callbackContext: CallbackContext): Boolean {
+    val properties = parameters.readProperties()
+    MIClient.categoryViewed(properties)
+    val pluginResult = PluginResult(PluginResult.Status.OK)
+    pluginResult.keepCallback = false
+    callbackContext.sendPluginResult(pluginResult)
+    return true
+  }
+
+  private fun productSearched(parameters: JSONArray): Boolean {
+    val properties = parameters.readProperties()
+    MIClient.productSearched(properties)
+    return true
+  }
+  private fun productAdded(parameters: JSONArray): Boolean {
+    val properties = parameters.readProperties()
+    MIClient.productAdded(properties)
+    return true
+  }
   private fun identifyUser(): Boolean {
     MIClient.identifyUser()
     return true
   }
-  
+  private fun lastResolvedUrl(callbackContext: CallbackContext): Boolean {
+    val url = MIClient.retrieveStoredDeepLink()
+    val result = PluginResult(PluginResult.Status.OK, url)
+    result.keepCallback = false
+
+    callbackContext.sendPluginResult(result)
+    return true
+  }
   private fun logEvent(parameters: JSONArray): Boolean {
     val eventName: String? = try {
       parameters.getString(0)
@@ -128,50 +158,15 @@ class MovablePlugin: CordovaPlugin {
       e.printStackTrace()
       return true
     }
-    
     val eventProperties = parameters.readProperties()
-    
     if (eventName.isNullOrEmpty()) {
       return true
     }
-    
-    MIClient.logEvent(eventName,eventProperties)
+    MIClient.logEvent(eventName, eventProperties)
     return true
   }
-  
-  private fun productSearched(parameters: JSONArray): Boolean {
-    val properties = parameters.readProperties()
-    MIClient.productSearched(properties)
-    return true
-  }
-  
-  private fun productViewed(parameters: JSONArray): Boolean {
-    val properties = parameters.readProperties()
-    MIClient.productViewed(properties)
-    return true
-  }
-  
-  private fun productAdded(parameters: JSONArray): Boolean {
-    val properties = parameters.readProperties()
-    MIClient.productAdded(properties)
-    return true
-  }
-  
-  private fun categoryViewed(parameters: JSONArray): Boolean {
-    val properties = parameters.readProperties()
-    MIClient.categoryViewed(properties)
-    return true
-  }
-  
-  private fun orderCompleted(parameters: JSONArray): Boolean {
-    val properties = parameters.readProperties()
-    MIClient.orderCompleted(properties)
-    return true
-  }
-  
   private fun JSONArray.readProperties(): Map<String, Any?> {
     val map = HashMap<String, Any?>()
-    
     try {
       for (i in 0 until length()) {
         val key = getString(i)
@@ -181,32 +176,7 @@ class MovablePlugin: CordovaPlugin {
     } catch (e: JSONException) {
       e.printStackTrace()
     }
-    
+
     return map
-  }
-  
-  private fun lastResolvedUrl(parameters: JSONArray, callbackContext: CallbackContext): Boolean {
-    val url = MIClient.retrieveStoredDeepLink()
-    val result = PluginResult(PluginResult.Status.OK, url)
-    callbackContext.sendPluginResult(result)
-    return true
-  }
-  
-  private fun resolveUrl(parameters: JSONArray, callbackContext: CallbackContext): Boolean {
-    val rawURL: String? = try {
-      parameters.getString(0)
-    } catch (e: JSONException) {
-      e.printStackTrace()
-      return true
-    }
-    
-    rawURL?.let {
-      MIClient.resolveUrlAsync(it) { resolvedURL ->
-        val result = PluginResult(PluginResult.Status.OK, resolvedURL)
-        callbackContext.sendPluginResult(result)
-      }
-    }
-    
-    return true
   }
 }
