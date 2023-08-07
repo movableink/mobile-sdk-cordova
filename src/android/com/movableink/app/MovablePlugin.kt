207c2c583177c0f4d6ef6df440aaf73a36331aa7
Chayel J Heinsen <chayelheinsen@users.noreply.github.com>
2023-08-07 16:03:44 -0400

    iOS and Android Support [sc-85914] (#1)

---
+++ src/android/com/movableink/app/MovablePlugin.kt
@@ -0,0 +1,216 @@
+
+package android.com.movableink.app
+
+import android.content.Intent
+import android.util.Log
+import com.movableink.app.*
+import com.movableink.inked.MIClient
+import org.apache.cordova.CallbackContext
+import org.apache.cordova.CordovaInterface
+import org.apache.cordova.CordovaPlugin
+import org.apache.cordova.CordovaWebView
+import org.apache.cordova.PluginResult
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
+const val TAG = "MovablePlugin"
+
+class MovablePlugin : CordovaPlugin() {
+  private var deepLinkListener: CallbackContext? = null
+
+  override fun initialize(cordova: CordovaInterface?, webView: CordovaWebView?) {
+    super.initialize(cordova, webView)
+    handleIntent(cordova?.activity?.intent)
+    MIClient.start()
+  }
+
+  private fun handleIntent(intent: Intent?) {
+    intent?.let {
+      if (it.action == Intent.ACTION_VIEW) {
+        val url = it.data.toString()
+        resolveURL(url)
+      }
+    }
+  }
+  override fun onNewIntent(intent: Intent?) {
+    super.onNewIntent(intent)
+    cordova.activity.intent = intent
+    handleIntent(intent)
+  }
+
+  private fun resolveURL(url: String) {
+    MIClient.resolveUrlAsync(url) { urlString ->
+      urlString?.let {
+        Log.d(TAG, "Resolved URL:$urlString ")
+        val result = PluginResult(PluginResult.Status.OK, it)
+        result.keepCallback = true
+        deepLinkListener?.sendPluginResult(result)
+      }
+
+    }
+  }
+
+  override fun execute(
+    action: String,
+    args: JSONArray,
+    callbackContext: CallbackContext,
+  ): Boolean {
+    when (action) {
+      START -> {
+        return start(callbackContext)
+      }
+      SET_MIU -> {
+        return setMiu(args)
+      }
+      ORDER_COMPLETED -> {
+        return orderCompleted(args, callbackContext)
+      }
+      PRODUCT_VIEWED -> {
+        return productViewed(args, callbackContext)
+      }
+      CATEGORY_VIEWED -> {
+        return categoryViewed(args, callbackContext)
+      }
+      PRODUCT_SEARCHED -> {
+        return productSearched(args)
+      }
+      PRODUCT_ADDED -> {
+        return productAdded(args)
+      }
+      IDENTIFY_USER -> {
+        return identifyUser()
+      }
+      RETRIEVE_LAST_RESOLVED_URL -> {
+        return lastResolvedUrl(callbackContext)
+      }
+      LOG_EVENT -> {
+        return logEvent(args)
+      }
+    }
+    return false
+  }
+
+  private fun setMiu(parameters: JSONArray): Boolean {
+    val miu: String? = try {
+      parameters.getString(0)
+    } catch (e: JSONException) {
+      e.printStackTrace()
+      return true
+    }
+    if (miu.isNullOrEmpty()) {
+      return true
+    }
+    MIClient.setMIU(miu)
+    return true
+  }
+  private fun orderCompleted(parameters: JSONArray, callbackContext: CallbackContext): Boolean {
+    val properties = parameters.readProperties()
+    MIClient.orderCompleted(properties)
+    val pluginResult = PluginResult(PluginResult.Status.OK)
+    pluginResult.keepCallback = false
+    callbackContext.sendPluginResult(pluginResult)
+    return true
+  }
+  private fun productViewed(parameters: JSONArray, callbackContext: CallbackContext): Boolean {
+    val properties = parameters.readProperties()
+    MIClient.productViewed(properties)
+    val pluginResult = PluginResult(PluginResult.Status.OK)
+    pluginResult.keepCallback = false
+    callbackContext.sendPluginResult(pluginResult)
+    return true
+  }
+  private fun categoryViewed(parameters: JSONArray, callbackContext: CallbackContext): Boolean {
+    val properties = parameters.readProperties()
+    MIClient.categoryViewed(properties)
+    val pluginResult = PluginResult(PluginResult.Status.OK)
+    pluginResult.keepCallback = false
+    callbackContext.sendPluginResult(pluginResult)
+    return true
+  }
+
+  private fun productSearched(parameters: JSONArray): Boolean {
+    val properties = parameters.readProperties()
+    MIClient.productSearched(properties)
+    return true
+  }
+  private fun productAdded(parameters: JSONArray): Boolean {
+    val properties = parameters.readProperties()
+    MIClient.productAdded(properties)
+    return true
+  }
+  private fun identifyUser(): Boolean {
+    MIClient.identifyUser()
+    return true
+  }
+  private fun lastResolvedUrl(callbackContext: CallbackContext): Boolean {
+    val url = MIClient.retrieveStoredDeepLink()
+    val result = PluginResult(PluginResult.Status.OK, url)
+    result.keepCallback = false
+
+    callbackContext.sendPluginResult(result)
+    return true
+  }
+  private fun logEvent(parameters: JSONArray): Boolean {
+    val eventName: String? = try {
+      parameters.getString(0)
+    } catch (e: JSONException) {
+      e.printStackTrace()
+      return true
+    }
+    val eventProperties = parameters.readProperties()
+    if (eventName.isNullOrEmpty()) {
+      return true
+    }
+    MIClient.logEvent(eventName, eventProperties)
+    return true
+  }
+  private fun start(callback: CallbackContext): Boolean {
+    deepLinkListener = callback
+    return true
+  }
+  private fun JSONArray.readProperties(): Map<String, Any?> {
+    val map = HashMap<String, Any?>()
+    try {
+      val jsonString = this.getString(0)
+      val jsonObject = JSONObject(jsonString)
+      return jsonObject.toMap()
+    } catch (e: Exception) {
+      e.printStackTrace()
+    }
+
+    return map
+  }
+
+  @Throws(JSONException::class)
+  fun JSONObject.toMap(): Map<String, Any?> {
+    val map: MutableMap<String, Any> = HashMap()
+    val keys: Iterator<String> = keys()
+    while (keys.hasNext()) {
+      val key = keys.next()
+      var value: Any = get(key)
+      if (value is JSONArray) {
+        value = toList(value as JSONArray)
+      } else if (value is JSONObject) {
+        value = (value as JSONObject).toMap()
+      }
+      map[key] = value
+    }
+    return map
+  }
+
+  @Throws(JSONException::class)
+  fun toList(array: JSONArray): List<Any?> {
+    val list: MutableList<Any> = ArrayList()
+    for (i in 0 until array.length()) {
+      var value: Any = array.get(i)
+      if (value is JSONArray) {
+        value = toList(value)
+      } else if (value is JSONObject) {
+        value = value.toMap()
+      }
+      list.add(value)
+    }
+    return list
+  }
+}
---
+++ src/ios/MovableInkClient.swift
@@ -0,0 +1,179 @@
+import Foundation
+import MovableInk
+
+@objc(MovableInkClient)
+class MovableInkClient: CDVPlugin {
+  
+  enum Errors: Error {
+    case invalidArguments
+  }
+  
+  private var deeplinkListener: String?
+  
+  override func pluginInitialize() {
+    super.pluginInitialize()
+    
+    MIClient.start { [weak self] result in
+      guard let self, let deeplinkListener = self.deeplinkListener else { return }
+      
+      switch result {
+      case let .success(url):
+        let pluginResult = CDVPluginResult(
+          status: CDVCommandStatus_OK,
+          messageAs: url
+        )
+        
+        pluginResult?.setKeepCallbackAs(true)
+        self.commandDelegate?.send(pluginResult, callbackId: deeplinkListener)
+        
+      default:
+        break
+      }
+    }
+  }
+  
+  @objc(start:)
+  func start(command: CDVInvokedUrlCommand) {
+    deeplinkListener = command.callbackId
+  }
+
+  @objc(retrieveStoredDeeplink:)
+  func retrieveStoredDeeplink(command: CDVInvokedUrlCommand) {
+    let pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_OK,
+      messageAs: MIClient.storedDeeplink ?? ""
+    )
+    
+    self.commandDelegate?.send(pluginResult, callbackId: command.callbackId)
+  }
+  
+  @objc(resolveUrl:)
+  func resolveUrl(command: CDVInvokedUrlCommand) {
+    let callbackId = command.callbackId
+    
+    guard let urlString = command.argument(at: 0) as? String, let url = URL(string: urlString) else {
+      let pluginResult = CDVPluginResult(
+        status: CDVCommandStatus_ERROR
+      )
+      
+      self.commandDelegate?.send(pluginResult, callbackId: callbackId)
+      
+      return
+    }
+                                                        
+    Task {
+      do {
+        let resolved = try await MIClient.resolve(url: url)
+        let pluginResult = CDVPluginResult(
+          status: CDVCommandStatus_OK,
+          messageAs: resolved.absoluteString
+        )
+        
+        self.commandDelegate?.send(pluginResult, callbackId: callbackId)
+      } catch {
+        let pluginResult = CDVPluginResult(
+          status: CDVCommandStatus_ERROR
+        )
+        
+        self.commandDelegate?.send(pluginResult, callbackId: callbackId)
+      }
+    }
+  }
+  
+  private func guardProperties(command: CDVInvokedUrlCommand) throws -> [String: Any] {
+    guard let properties = command.argument(at: 0) as? [String: Any] else {
+      let pluginResult = CDVPluginResult(
+        status: CDVCommandStatus_ERROR
+      )
+      
+      self.commandDelegate?.send(pluginResult, callbackId: command.callbackId)
+      
+      throw Errors.invalidArguments
+    }
+    
+    return properties
+  }
+  
+  @objc(productSearched:)
+  public func productSearched(command: CDVInvokedUrlCommand) {
+    guard let properties = try? guardProperties(command: command) else {
+      return
+    }
+    
+    MIClient.productSearched(properties)
+  }
+  
+  @objc(productViewed:)
+  public func productViewed(command: CDVInvokedUrlCommand) {
+    guard let properties = try? guardProperties(command: command) else {
+      return
+    }
+    
+    MIClient.productViewed(properties)
+  }
+  
+  @objc(productAdded:)
+  public func productAdded(command: CDVInvokedUrlCommand) {
+    guard let properties = try? guardProperties(command: command) else {
+      return
+    }
+    
+    MIClient.productAdded(properties)
+  }
+  
+  @objc(orderCompleted:)
+  public func orderCompleted(command: CDVInvokedUrlCommand) {
+    guard let properties = try? guardProperties(command: command) else {
+      return
+    }
+    
+    MIClient.orderCompleted(properties)
+  }
+  
+  @objc(categoryViewed:)
+  public func categoryViewed(command: CDVInvokedUrlCommand) {
+    guard let properties = try? guardProperties(command: command) else {
+      return
+    }
+    
+    MIClient.categoryViewed(properties)
+  }
+  
+  @objc(logEvent:)
+  public func logEvent(command: CDVInvokedUrlCommand) {
+    guard let name = command.argument(at: 0) as? String,
+          let properties = command.argument(at: 1) as? [String: Any]
+    else {
+      let pluginResult = CDVPluginResult(
+        status: CDVCommandStatus_ERROR
+      )
+      
+      self.commandDelegate?.send(pluginResult, callbackId: command.callbackId)
+      
+      return
+    }
+    
+    MIClient.logEvent(name: name, properties: properties)
+  }
+  
+  @objc(identifyUser:)
+  public func identifyUser(command: CDVInvokedUrlCommand) {
+    MIClient.identifyUser()
+  }
+  
+  @objc(setMIU:)
+  public func setMIU(command: CDVInvokedUrlCommand) {
+    guard let value = command.argument(at: 0) as? String
+    else {
+      let pluginResult = CDVPluginResult(
+        status: CDVCommandStatus_ERROR
+      )
+      
+      self.commandDelegate?.send(pluginResult, callbackId: command.callbackId)
+      
+      return
+    }
+    
+    MIClient.setMIU(value)
+  }
+}
